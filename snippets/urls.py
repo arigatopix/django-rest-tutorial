ce82da44b9ac5de20d7c1e5c17e1e3d9de3ee8b9
arigatopix <teeruch.k@gmail.com>
2019-06-21 17:30:04 +0700

    Schemas

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from snippets import views
 
 # ใช้ DefaultRouter เพื่อ Bind และคิดให้อัตโนมัติว่า pattern จะเป็นยังไง
@@ -14,7 +15,12 @@ router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+# core schema เอาไว้ดู tree ของ api
+schema_view = get_schema_view(title='Pastebin API')
+
+
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('', include(router.urls)),
+    path('schema/', schema_view),
 ]
